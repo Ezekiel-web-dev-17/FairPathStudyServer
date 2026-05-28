7f5422b5403972f7b5f6cb770e345a1c38bec17f
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-05-28 22:11:55 +0100

    feat(db): add user email verification fields and configure dynamic env loading in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,15 @@
-import 'dotenv/config';
+import { config } from 'dotenv';
 import { defineConfig } from 'prisma/config';
+import process from 'node:process';
+
+let envPath = '.env.development.local';
+if (process.env.NODE_ENV === 'production') {
+  envPath = '.env.production.local';
+} else if (process.env.NODE_ENV === 'test') {
+  envPath = '.env.test.local';
+}
+
+config({ path: envPath });
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
